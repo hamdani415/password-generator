e75c3374c02ab6bb4afebc9fa831445974f4715c
hamdani <neswakun27@gmail.com>
2025-07-20 01:17:28 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const angka = '1234567890'
+const hurufKecil = 'abcdefghijklmnopqrstuvwxyz'
+const hurufKapital = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const simbol = '!@#$%^&*()_+'
+
+const data = angka + hurufKecil + hurufKapital + simbol
+let pw = ''
+
+const panjang = document.getElementById('panjang')
+const password = document.getElementById('password')
+// const generate = document.getElementById('generate')
+
+function generate (){
+    alert('password berhasil di generate')
+    for (let i = 0 ; i < panjang.value ; i ++){
+        pw += data[~~(Math.random() * data.length)]
+    }
+    password.value = pw
+}
